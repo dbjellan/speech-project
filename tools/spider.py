a185f84f1051b557abfb8b85e0ca2e4b89ab7470
dbjellan <dbjellan@macalester.edu>
2016-10-27 17:39:35 -0500

    scraper for voxforge

---
+++ tools/spider.py
@@ -0,0 +1,32 @@
+import urlparse
+import os
+
+from scrapy.http import Request
+from scrapy.spider import BaseSpider
+
+project_directory = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+save_directory = os.path.join(project_directory, 'corpus/download')
+extract_directory = os.path.join(project_directory, 'corpus/extracted')
+os.chdir(extract_directory)
+
+
+class VoxSpeechSpider(BaseSpider):
+    name = "voxspider"
+    start_urls = ['http://www.repository.voxforge1.org/downloads/SpeechCorpus/Trunk/Audio/Main/16kHz_16bit/']
+
+    def parse(self, response):
+        base_url = 'http://www.repository.voxforge1.org/downloads/SpeechCorpus/Trunk/Audio/Main/16kHz_16bit/'
+        for a in response.xpath('//a[@href]/@href'):
+            link = a.extract()
+            if link.endswith('.tgz'):
+                link = urlparse.urljoin(base_url, link)
+                yield Request(link, callback=self.save_file)
+
+    def save_file(self, response):
+        name = response.url.split('/')[-1]
+        print('Downloading and extraction: %s' % (name, ))
+        path = os.path.join(save_directory, name)
+        with open(path, 'wb') as f:
+            f.write(response.body)
+        os.system('tar -xvf ../download/%s' % (name,))
+
